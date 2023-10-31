211fb8ca76d9637dd74291e3ce135d959d118299
Berkay Yıldız <berkay4421@gmail.com>
2023-10-31 16:38:44 +0300

    Create forgot password page and change some common css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpInformationsComponent } from './pages/sign-up-informations/sign-up-informations.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
 const routes: Routes = [
+  {path:'forgot-password', component: ForgotPasswordComponent},
   {path:'sign-up-informations', component: SignUpInformationsComponent},
   {path:'sign-in', component: SignInComponent},
   {path:'sign-up', component: SignUpComponent},
--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent {
+export class ForgotPasswordComponent implements OnInit {
+  formError: any;
+  formPasswordError: any;
+  submitted = false;
 
+  forgotPasswordAnimation: Record<string, any> = {
+    path: '/assets/json/forgot_password_animation.json', // Path to your animation JSON file
+    renderer: 'svg', // Use 'canvas' or 'html' for different renderers
+    loop: true,
+    autoplay: true,
+  };
+
+  forgotPassword = new UntypedFormGroup({
+    email: new UntypedFormControl('', [
+      Validators.required,
+    ]),
+    password: new UntypedFormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]),
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+  }
 }
