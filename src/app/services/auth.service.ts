b4081d05521e472f60fec908a405e96141762409
Berkay Yıldız <berkay4421@gmail.com>
2024-02-24 17:34:21 +0300

    Fix double request

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CustomResponse } from '../models/custom-response';
-import { Observable } from 'rxjs';
 import { AdditionalFieldsRequestCommand, SendOtpVerificationRequestCommand, SignUpRequestCommand } from '../models/commands/auth-request-commands';
 import { RequestService } from './base/request.service';
 
@@ -12,18 +10,15 @@ export class AuthService {
 
     userEmail: string;
     userPassword: string;
-    baseUrl = 'https://localhost:7044/api/v1/user';
 
-    constructor(private httpClient: HttpClient, private request: RequestService) {
-
-    }
+    constructor(private request: RequestService) { }
 
     async signUp(command: SignUpRequestCommand): Promise<CustomResponse<string>> {
         return await this.request.send("/auth/sign-up", command);
     }
 
     async sendOtpVerification(command: SendOtpVerificationRequestCommand): Promise<CustomResponse<string>> {
-        return await this.request.send("/auth/send-otp-verification",command);
+        return await this.request.send("/auth/send-otp-verification", command);
     }
 
     async additinalFields(command: AdditionalFieldsRequestCommand): Promise<CustomResponse<string>> {
